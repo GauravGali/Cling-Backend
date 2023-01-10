ef5cab909692337c0ceaa3875b79ab6fe6b28b73
GauravGali <101819090+GauravGali@users.noreply.github.com>
2023-01-10 16:21:40 +0530

    Requirements Deploy Commit_

--- cling/settings.py
+++ cling/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=#h_-c$i-q(@90wlvz8qsh^+suug8m_&sb*uc2#52rb8%c1wum'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.pythonanywhere.com', 'localhost:8000']
 
 
 # Application definition
