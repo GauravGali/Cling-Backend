24457e2f7ef4a35453873087489051de1b5bd706
GauravGali <101819090+GauravGali@users.noreply.github.com>
2023-01-11 00:50:23 +0530

    Host Updated Deploy Commit___

--- cling/settings.py
+++ cling/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-=#h_-c$i-q(@90wlvz8qsh^+suug8m_&sb*uc2#52rb8%c1wum
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', 'Cling.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
